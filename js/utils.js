71c07364bbd1eb1a31ebf6ef10a1a0825b75bfc7
Joanna <joannabraccini@gmail.com>
2024-07-18 11:49:16 -0300

    fix index page size

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
 function alertToast(message, type) {
-  const container = document.getElementById("container-toast")
+  const container = document.getElementById("container-toast");
 
   container.innerHTML = `        
       <div class="toast align-items-center text-bg-${type}" id="toast" role="alert" aria-live="assertive" aria-atomic="true">
@@ -9,17 +9,17 @@ function alertToast(message, type) {
               </div>
               <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
           </div>
-      </div>`
+      </div>`;
 
-  const toastNotification = new bootstrap.Toast("#toast")
-  toastNotification.show()
+  const toastNotification = new bootstrap.Toast("#toast");
+  toastNotification.show();
 }
 
 function showLoading(show) {
-  const listCards = document.getElementById('list-cards')
+  const listCards = document.getElementById("list-cards");
 
   if (show) {
-      listCards.innerHTML = `
+    listCards.innerHTML = `
       <div class="d-flex align-items-center justify-content-start">
           <h5 class="fst-italic fs-2 text-success me-2">
           Buscando Informações
@@ -27,8 +27,8 @@ function showLoading(show) {
           <span class="spinner-border spinner-border-sm text-light" aria-hidden="true"></span>
           </div>
       </div>
-      `
+      `;
   } else {
-      listCards.innerHTML = ''
+    listCards.innerHTML = "";
   }
 }
